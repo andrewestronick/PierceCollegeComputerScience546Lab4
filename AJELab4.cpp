1595f65623855198cb1a24d520ae98ecedbf0e27
andrewestronick <andy.estronick@hotmail.com>
2017-05-03 22:31:37 -0700

    Working in C++

--- AJELab4.cpp
+++ AJELab4.cpp
@@ -1,25 +1,20 @@
 // Andrew Estronick
-
+#define _USE_MATH_DEFINES
 
 #include<iostream>
 #include<vector>
 #include<fstream>
 #include<sstream>
-#include <algorithm>
+#include<algorithm>
+#include <cmath>
 
-class record
+
+struct record
 {
-public:
-	record(unsigned ID, float latitude, float longitude);
-	bool compareLatitude(const record &a, const record &b);
-	bool compareLongitude(const record &a, const record &b);
-	unsigned getID();
-	float getLatitude();
-	float getLongitude();
-private:
 	unsigned ID;
 	float latitude;
 	float longitude;
+	float distance;
 };
 
 int main(int argc, char *argv[])
@@ -44,66 +39,111 @@ int main(int argc, char *argv[])
 	while (getline(fin, line))
 	{
 		std::stringstream buffer(line);
-		int ID;
+		unsigned ID;
 		float latitude, longitude;
+
 		buffer >> ID;
 		buffer >> latitude;
 		buffer >> longitude;
 
+		if (0 == latitude && 0 == longitude)
+			continue;
 
-		coordinates.emplace_back(record(ID, latitude, longitude));
+		coordinates.emplace_back(record{ ID, latitude, longitude, 0.0 });
 	}
 
 	size_t size = coordinates.size();
 
-	// std::sort(coordinates.begin(), coordinates.end(), [](record &a, record &b) { return a.getLatitude() < b.getLatitude(); });
-
-	for (unsigned i = 0; i < size; ++i)
-	{
-		std::cout << i << " " << coordinates[i].getID() << " " << coordinates[i].getLatitude() << ", " << coordinates[i].getLongitude() << std::endl;
-	}
-
+	// std::sort(coordinates.begin(), coordinates.end(), [](record &a, record &b) { return a.latitude < b.latitude; });
 
 	unsigned *ID = new unsigned[size];
 	float *latitude = new float[size];
 	float *longitude = new float[size];
 
+	/*
 	for (unsigned i = 0; i < size; ++i)
 	{
-		ID[i] = coordinates[i].getID();
-		latitude[i] = coordinates[i].getLatitude();
-		longitude[i] = coordinates[i].getLongitude();
+		ID[i] = coordinates[i].ID;
+		latitude[i] = coordinates[i].latitude;
+		longitude[i] = coordinates[i].longitude;
+	}
+	*/
+
+	while (true)
+	{
+		std::cout << "Enter input location (latitude and longitude), or -999 -999 to end. ";
+
+		float latitude, longitude;
+
+		std::cin >> latitude;
+		std::cin >> longitude;
+
+		if (latitude <= -999 && longitude <= -999)
+			break;
+
+		if (latitude < -90 || latitude > 90 || longitude < -180 || longitude > 180)
+		{
+			std::cout << "Values " << latitude << " and " << longitude << " are not acceptable, re-enter.\n";
+			continue;
+		}
+
+		for (record &r : coordinates)
+		{
+			float lat1 = latitude;
+			float long1 = longitude;
+			float lat2 = r.latitude;
+			float long2 = r.longitude;
+
+			float PI = M_PI;
+
+			float dlat1 = lat1*(PI / 180);
+
+			float dlong1 = long1*(PI / 180);
+			float dlat2 = lat2*(PI / 180);
+			float dlong2 = long2*(PI / 180);
+
+			float dLong = dlong1 - dlong2;
+			float dLat = dlat1 - dlat2;
+
+			float aHarv = pow(sin(dLat / 2.0), 2.0) + cos(dlat1)*cos(dlat2)*pow(sin(dLong / 2), 2);
+			float cHarv = 2 * atan2(sqrt(aHarv), sqrt(1.0 - aHarv));
+		
+			const float earth = 3963.19;
+			r.distance = earth*cHarv;
+		}
+
+		std::sort(coordinates.begin(), coordinates.end(), [](record &a, record &b) { return a.distance < b.distance; });
+
+		for (record &r : coordinates)
+		{
+			std::cout << r.ID << "::" << r.latitude << ", " << r.longitude << ", " << r.distance << "\n";
+		}
+		/*
+		double PI = M_PI;
+
+		double dlat1 = lat1*(PI / 180);
+
+		double dlong1 = long1*(PI / 180);
+		double dlat2 = lat2*(PI / 180);
+		double dlong2 = long2*(PI / 180);
+
+		double dLong = dlong1 - dlong2;
+		double dLat = dlat1 - dlat2;
+
+		double aHarv = pow(sin(dLat / 2.0), 2.0) + cos(dlat1)*cos(dlat2)*pow(sin(dLong / 2), 2);
+		double cHarv = 2 * atan2(sqrt(aHarv), sqrt(1.0 - aHarv));
+		//earth's radius from wikipedia varies between 6,356.750 km - 6,378.135 km (~3,949.901 - 3,963.189 miles)
+		//The IUGG value for the equatorial radius of the Earth is 6378.137 km (3963.19 mile)
+		const double earth = 3963.19;//I am doing miles, just change this to radius in kilometers to get distances in km
+		double distance = earth*cHarv;
+
+		cout << ''...Distance is '' << distance << '' miles.\n'';
+		*/
 	}
 
-	std::cout << "Size = " << size << std::endl;
+	std::cout << "Ending execution. \n";
 
-}
-
-record::record(unsigned ID, float latitude, float longitude)
-{
-}
-
-bool record::compareLatitude(const record & a, const record & b)
-{
-	return a.latitude < b.latitude;
-}
-
-bool record::compareLongitude(const record & a, const record & b)
-{
-	return a.longitude < b.longitude;
-}
-
-unsigned record::getID()
-{
-	return ID;
-}
-
-float record::getLatitude()
-{
-	return latitude;
-}
-
-float record::getLongitude()
-{
-	return longitude;
+	delete ID;
+	delete latitude;
+	delete longitude;
 }
